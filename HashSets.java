4601032167a86986918e1e76480ef1e4379c2290
psvsai <pasupuletisvsm245@gmail.com>
2024-08-13 19:51:55 +0530

    PSVSAI

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,15 @@
+import java.util.HashSet;
+
+
 public class HashSets{
 // Prepare Hashset object here and add person class objects in them iterate it by using for each
+public static void main(String[] args) {
+   HashSet<Person> i = new HashSet<>();
+   i.add(new Person("PSVSAI", 3, 15000, "Associate"));
+   i.add(new Person("MAHESH", 34, 25000, "TL"));
+   i.add(new Person("SAI MAHESH", 25, 35000, "B3"));
+    for(Person person:i){
+        System.out.println(person);
+    }
+}
 }
--- LinkedHashSets.java
+++ LinkedHashSets.java
@@ -1,4 +1,17 @@
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
 public class LinkedHashSets{
 public static void main(String[]args){
   // Preapre LinkedHashSets Object and Load Person Objects in it and iterate it using Iterator and While Loop
+ LinkedHashSet<Person> a = new LinkedHashSet<>();
+ a.add(new Person("SRI VENKATA", 3, 18000, "Assoicate"));
+ a.add(new Person("SAI MAHESH", 30, 25550, "TL"));
+ a.add(new Person("PSVSAI", 345, 35000, "HR"));
+ Iterator<Person> p = a.iterator();
+  while (p.hasNext()){
+    System.out.println(p.next());
+    
+  }
+}
 }
